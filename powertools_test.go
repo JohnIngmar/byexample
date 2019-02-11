7f612c8351f0d0e9613a4cf3ed51004f7af99f0e
guni <guni.yankelovitz@oracle.com>
2019-02-11 10:25:11 +0200

    Fix issue

--- powertools_test.go
+++ powertools_test.go
@@ -16,21 +16,21 @@ func TestKeySetMap(t *testing.T) {
 	set3["a"] = 1
 
 	value := "val1"
-	map1.set(set1, value)
+	map1.Set(set1, value)
 
-	val1, _ := map1.get(set1)
+	val1, _ := map1.Get(set1)
 	if val1 != value {
 		t.Fatalf("bad, Expected values to be the same: %s=%s", val1, value)
 	}
 
-	val2, _ := map1.get(set2)
+	val2, _ := map1.Get(set2)
 	if val1 != val2 {
 		t.Fatalf("bad, Expected values to be the same: %s=%s", val1, val2)
 	}
 
 	delete(map1, set3)
 
-	val3, _ := map1.get(set1)
+	val3, _ := map1.Get(set1)
 
 	if val3 != nil {
 		t.Fatalf("bad, Expected value to be removed: %s", val3)
