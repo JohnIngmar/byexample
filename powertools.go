67226bc9277eef5ed92715f49d96825ab15d01ef
guni <guni.yankelovitz@oracle.com>
2019-02-11 10:34:38 +0200

    Fix issue

--- powertools.go
+++ powertools.go
@@ -10,7 +10,7 @@ type PowerMap map[uint64]interface{}
 
 func (setMap *PowerMap) Set(key interface{}, value interface{}) (keyHash uint64, err error) {
 	keyHash, err = hashstructure.Hash(key, nil)
-	if err != nil {
+	if err == nil {
 		(*setMap)[keyHash] = value
 	}
 	return
@@ -18,7 +18,7 @@ func (setMap *PowerMap) Set(key interface{}, value interface{}) (keyHash uint64,
 
 func (setMap *PowerMap) Get(key interface{}) (interface{}, error) {
 	keyHash, err := hashstructure.Hash(key, nil)
-	if err != nil {
+	if err == nil {
 		return (*setMap)[keyHash], nil
 	}
 	return nil, err
@@ -26,7 +26,7 @@ func (setMap *PowerMap) Get(key interface{}) (interface{}, error) {
 
 func (setMap *PowerMap) Delete(key interface{}) error {
 	keyHash, err := hashstructure.Hash(key, nil)
-	if err != nil {
+	if err == nil {
 		delete(*setMap, keyHash)
 	}
 	return err
